18e452bf4ad8639c95693ad3cc778e35f77ef07c
madyar997 <madiar.997@gmail.com>
2021-07-12 13:07:12 +0600

    fixed deployment error

--- src/main/java/newsportal/SpringXMLConfigurationMain.java
+++ src/main/java/newsportal/SpringXMLConfigurationMain.java
@@ -4,7 +4,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringXMLConfigurationMain {
     public static void main(String[] args) {
-//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/WEB-INF/NewsPortal-servlet.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/WEB-INF/NewsPortal-servlet.xml");
 
     }
 }
